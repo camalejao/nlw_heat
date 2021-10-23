4228c2989159a1c221bd91205a374b985b567548
camalejao <jvfsl@ic.ufal.br>
2021-10-23 11:56:40 -0300

    fix server port variable

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,4 +1,4 @@
 import { httpServer } from "./app";
 
-const port = process.env.NODE_PORT || 4000;
+const port = process.env.PORT || 4000;
 httpServer.listen(port, () => { console.log(`server running on port ${port}`); });
